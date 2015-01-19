cf844b43260a08cd751f5c35b9279ea6c0506c19
Austen Rogers <rogersa2@wit.edu>
2015-01-19 15:04:37 -0500

    lab 1 is DOne

---
+++ lab1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+int i;
+ifstream MyFile;
+MyFile.open(numbers.txt);
+
+while (MyFile >> i) {
+cout << i;
+}
+
+
+
+
+}
+
