f551444b116f4b67554a92420c0f2e687dd0c61a
Austen Rogers <rogersa2@wit.edu>
2015-02-19 18:03:11 -0500

    Lab 1 done, sorta

--- lab1.cpp
+++ lab1.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <cmath>
+#include <math.h>
 using namespace std;
 
-int main(int argc, char* argv[])
-{
-int i;
-ifstream MyFile;
-MyFile.open(numbers.txt);
+int main(int argc, char* argv[]) {
+    ifstream file;
+    file.open(argv[1]);
+   
+    if (file.fail()) {
+            cout << "Enter A File" << endl;
+            return 1;
+    }
+    float val, squared, sum;
+    while (file >> val) {
+        cout << val << "\t" << squared << "\t" << sum << endl;
+        squared = (val + 1) * (val + 1);
+        sum = (val + 1) + squared;
+    }
 
-while (MyFile >> i) {
-cout << i;
+    file.close();
+    return 0;
 }
-
-
-
-
-}
-
